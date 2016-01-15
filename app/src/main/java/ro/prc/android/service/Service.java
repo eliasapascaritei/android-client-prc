8263ebdb7605f98576929e5ba9a9fe0544595d43
Sofronia Ciprian <sofronia.ciprian@gmail.com>
2016-01-16 00:05:52 +0200

    second commit

---
+++ app/src/main/java/ro/prc/android/service/Service.java
@@ -0,0 +1,41 @@
+package ro.prc.android.service;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class Service {
+
+    public static void verifyCNP(Context context, String CNP) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = "http://openapi.ro/api/validate/cnp/" +  CNP + ".json";
+
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+                        Log.d("Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        );
+
+        queue.add(getRequest);
+    }
+
+}
